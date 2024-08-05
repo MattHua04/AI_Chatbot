983063402afb8d804be1f6fb5f3c923aa5571353
MattHua04 <matthewhua8@gmail.com>
2024-08-04 21:47:56 -0400

    save

--- chatbot/web/webInterfaceSupport.py
+++ chatbot/web/webInterfaceSupport.py
@@ -32,6 +32,13 @@ def postPromptResponse(messages, conversation, conversations):
     id = conversation["_id"]
     content = messages
     messages = copy.deepcopy(content)
+    last_message = messages[-1]
+    last_message_owner = last_message[0]
+    if last_message_owner == "AI" and last_message[1] == "..." and conversation['respond']:
+        content.pop()
+        messages.pop()
+        last_message = messages[-1]
+        last_message_owner = last_message[0]
     # Convert content to openai format
     messages = [{"role": "assistant" if message[0] == "AI" else "user", "content": message[1]} for message in messages]
     updated_content = copy.deepcopy(content)
@@ -44,16 +51,30 @@ def postPromptResponse(messages, conversation, conversations):
     else:
         conversations.update_one({"_id": id}, {"$set": {"content": updated_content, "compressed_content": ''}})
     # Send prompt to OpenAI
-    response = sendPrompt(messages)
-    updated_content = copy.deepcopy(content)
-    updated_content.append(["AI", response])
+    if last_message_owner == "User":
+        response = sendPrompt(messages)
+        updated_content = copy.deepcopy(content)
+        updated_content.append(["AI", response])
+    elif last_message_owner == "AI":
+        response = sendPrompt(messages,
+            """
+            You are a helpful assistant that will answer all the user's prompts to the best of your abilities. 
+            In this case, please continue the conversation in a way that expands and adds meaning to what has already been said. 
+            For example, if you've been telling a story, continue the story, or if you've been discussing a topic, 
+            begin discussing related more complex topics that build off what you've told the user. 
+            If you asked a question in your latest message that the user has not responded to, 
+            make a prediction of the user's response and continue the conversation based on that prediction.
+            Do not repeat the same information you have said before and determine what to discuss next by yourself without consulting the user.
+            """)
+        updated_content = copy.deepcopy(content)
+        updated_content[-1][1] += "\n" + response
     # Compress content before updating document in MongoDB
     should_compress = sys.getsizeof(json.dumps(updated_content)) > MAX_CONTENT_SIZE
     if should_compress: updated_content = compress_content(updated_content)
     if should_compress:
-        conversations.update_one({"_id": id}, {"$set": {"content": [], "compressed_content": updated_content}})
+        conversations.update_one({"_id": id}, {"$set": {"content": [], "compressed_content": updated_content, "respond": False}})
     else:
-        conversations.update_one({"_id": id}, {"$set": {"content": updated_content, "compressed_content": ''}})
+        conversations.update_one({"_id": id}, {"$set": {"content": updated_content, "compressed_content": '', "respond": False}})
 
 def findRemotePrompts(sp, pixels, lightsUsageStatus, sleepLightsState):
     # Remote conversation memory
@@ -72,16 +93,13 @@ def findRemotePrompts(sp, pixels, lightsUsageStatus, sleepLightsState):
                 conversationQueue = []
                 for conversation in all_conversations:
                     # Check for uncompressed conversations
-                    if len(conversation['content']) > 0:
-                        last_message_owner = conversation['content'][-1][0]
-                        if last_message_owner == "User":
+                    if conversation['respond']:
+                        if len(conversation['content']) > 0:
                             conversationQueue.append(conversation)
-                    # Check for compressed conversations
-                    elif conversation['compressed_content'] != '':
-                        conversation['compressed_content'] = decompress_content(conversation['compressed_content'])
-                        if conversation['compressed_content'] and len(conversation['compressed_content']) > 0:
-                            last_message_owner = conversation['compressed_content'][-1][0]
-                            if last_message_owner == "User":
+                        # Check for compressed conversations
+                        elif conversation['compressed_content'] != '':
+                            conversation['compressed_content'] = decompress_content(conversation['compressed_content'])
+                            if conversation['compressed_content'] and len(conversation['compressed_content']) > 0:
                                 conversationQueue.append(conversation)
             except:
                 db = MongoClient(MDB_CONN_STR, tlsCAFile=certifi.where())['test']
